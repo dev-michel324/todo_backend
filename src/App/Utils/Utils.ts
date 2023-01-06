de9b571f9d904d5e70a3d41f439a82d03ee296df
dev-michel324 <dev_michel324@protonmail.com>
2023-01-06 20:29:01 -0300

    feat: added Utils file that contain exist feature to verify if row exists in db

---
+++ src/App/Utils/Utils.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../Models/Todo";
+const dataSource = require("../../Database/dataSource");
+
+class Utils {
+
+
+    async exists(model: Todo, id: number): Promise<any> {
+        const objectFromDb = await dataSource
+            .getRepository(model)
+            .createQueryBuilder("object")
+            .where("object.id = :id", { id: id })
+            .getOne()
+
+        return objectFromDb;
+    }
+
+}
+
+module.exports = new Utils();
