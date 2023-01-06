3c5500e60ada1f480077df638c2ebe1fb71a1bf2
dev-michel324 <dev_michel324@protonmail.com>
2023-01-06 20:28:16 -0300

    feat: validator to tasks

---
+++ src/App/Validators/Todo.ts
@@ -0,0 +1,32 @@
+import {
+    object,
+    string,
+    number,
+    InferType,
+    boolean
+} from 'yup';
+
+const todoBaseSchema = object({
+    id: number().required().integer().positive()
+});
+
+const todoCreateSchema = object({
+    description: string().required().min(1).max(255),
+});
+
+const todoUpdateSchema = object({
+    open: boolean()
+}).concat(todoBaseSchema).concat(todoCreateSchema);
+
+type TodoBaseType = InferType<typeof todoBaseSchema>;
+type TodoUpdateType = InferType<typeof todoUpdateSchema>;
+type TodoCreateType = InferType<typeof todoCreateSchema>;
+
+export {
+    todoBaseSchema,
+    todoCreateSchema,
+    todoUpdateSchema,
+    TodoBaseType,
+    TodoCreateType,
+    TodoUpdateType
+};
