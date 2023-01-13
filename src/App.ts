0fa191909a5013859a0a50f1b748cf6ccb6c39ce
dev-michel324 <dev_michek324@protonmail.com>
2023-01-13 19:31:24 -0300

    feat: changes in route

--- src/App.ts
+++ src/App.ts
@@ -5,6 +5,9 @@ const port: number = 4444;
 
 app.get("/todo", TodoController.index);
 app.post("/todo", TodoController.create);
+app.get("/todo/show/:id", TodoController.show);
+app.put("/todo/:id", TodoController.update);
+app.delete("/todo/:id", TodoController.remove);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
