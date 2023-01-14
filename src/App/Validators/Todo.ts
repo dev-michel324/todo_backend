bbfbbdccc762e9cd0a60ca05020b802a32389a56
dev-michel324 <dev_michek324@protonmail.com>
2023-01-14 13:19:09 -0300

    removed from updateSchema the 'open' parameter

--- src/App/Validators/Todo.ts
+++ src/App/Validators/Todo.ts
@@ -1,32 +1,26 @@
-import {
-    object,
-    string,
-    number,
-    InferType,
-    boolean
-} from 'yup';
+import { object, string, number, InferType, boolean } from "yup";
 
 const todoBaseSchema = object({
-    id: number().required().integer().positive()
+  id: number().required().integer().positive(),
 });
 
 const todoCreateSchema = object({
-    description: string().required().min(1).max(255),
+  description: string().required().min(1).max(255),
 });
 
-const todoUpdateSchema = object({
-    open: boolean()
-}).concat(todoBaseSchema).concat(todoCreateSchema);
+const todoUpdateSchema = object({})
+  .concat(todoBaseSchema)
+  .concat(todoCreateSchema);
 
 type TodoBaseType = InferType<typeof todoBaseSchema>;
 type TodoUpdateType = InferType<typeof todoUpdateSchema>;
 type TodoCreateType = InferType<typeof todoCreateSchema>;
 
 export {
-    todoBaseSchema,
-    todoCreateSchema,
-    todoUpdateSchema,
-    TodoBaseType,
-    TodoCreateType,
-    TodoUpdateType
+  todoBaseSchema,
+  todoCreateSchema,
+  todoUpdateSchema,
+  TodoBaseType,
+  TodoCreateType,
+  TodoUpdateType
 };
